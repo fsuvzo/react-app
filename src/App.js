ab7282f7a14ce5be3dd4a6a7dfcbce21188b7b87
fsuvzo <felipesuvzo@gmail.com>
2025-02-21 14:35:13 -0300

    Actualización de .gitignore y cambios en App.js test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>¡React está bien configurado en mi servidor!</h1>
-        <p>Este es un ejemplo básico para verificar la configuración test .</p>
-      </header>
-    </div>
-  );
+
+    return (
+        <div style={{ padding: "20px" }}>
+            <p>esto es el test número 2</p>
+        </div>
+    );
 }
 
 export default App;
